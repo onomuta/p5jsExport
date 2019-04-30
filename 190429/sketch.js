5cb3b166131fe5080a0faa769c1e593db63a952a
Takao Onomura <onomuta@gmail.com>
2019-04-30 14:54:15 +0900

    no message

---
+++ 190429/sketch.js
@@ -0,0 +1,44 @@
+let saveFileName = ""
+let saveStart = false
+let startFrame = 0
+function preload(){
+}
+
+function setup() {
+  saveSetup()
+  // createCanvas(1280, 720, WEBGL);
+  createCanvas(1280, 720)
+  textSize(16)
+  stroke(255)
+  fill(200)
+}
+
+function draw() {
+  background(0)
+  rect(frameCount,100,10, 10)
+  text(frameCount,100,100)
+  saveFrame(10)
+}
+
+// S押して連番書き出し
+function saveSetup(){
+  var c = "abcdefghijklmnopqrstuvwxyz"
+  var cl = c.length
+  for(var i=0; i<4; i++){
+    saveFileName += c[Math.floor(Math.random()*cl)]
+  }
+}
+function saveFrame(duration){
+  if(saveStart == true){
+    if(frameCount<= startFrame + duration){
+      save(saveFileName + '_' + ( '000' + (frameCount - startFrame) ).slice( -4 ) + '.png')
+    }
+  }
+}
+function keyPressed(){
+  if(key == "S"){
+    console.log(key)
+    saveStart = true
+    startFrame = frameCount
+  }
+}
